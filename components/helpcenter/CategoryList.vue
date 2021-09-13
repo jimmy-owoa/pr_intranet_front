7ed16eb0e0e0728882a6ba48be62ff5465044ae7
javierc22 <javier.ec.22@gmail.com>
2021-09-13 11:22:30 -0300

    Add component CategoryList

---
+++ components/helpcenter/CategoryList.vue
@@ -0,0 +1,41 @@
+<template lang="">
+  <v-row>
+    <v-col cols="12" md="6" v-for="category in categories" :key="category.id">
+      <v-card 
+        :class="`cyan--text pointer mb-2 hc__rounded-card py-3 ${isMobile ? '' : 'mr-2'}`" 
+        max-width="600" 
+        @click="$router.push(`/centro-de-ayuda/${category.slug}`)"
+      >
+        <v-row align="center" no-gutters>
+          <v-col cols="2">
+            <v-img
+              :src="category.image"
+              height="40px"
+              contain
+            ></v-img>
+          </v-col>
+          <v-col cols="10">
+            <v-card-title>
+              <p class="title hc__blue-text ma-0">{{ category.name }}</p>
+            </v-card-title>
+          </v-col>
+        </v-row>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+<script>
+export default {
+  props: {
+    categories: { type: Array, default: () => [] }
+  },
+  computed: {
+    isMobile() {
+      return this.$vuetify.breakpoint.mobile
+    }
+  }
+}
+</script>
+<style lang="">
+  
+</style>
