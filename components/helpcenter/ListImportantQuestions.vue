48a36dcbbebb7673bbebc58f6137b19c9295c20e
javierc22 <javier.ec.22@gmail.com>
2021-09-13 11:22:42 -0300

    Add component ListImportantQuestions

---
+++ components/helpcenter/ListImportantQuestions.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-card class="hc__rounded-card">
+    <v-list v-for="(question, i) in questions" :key="i"> 
+      <v-list-item
+        :key="i"
+        :to="`${question.url}`"
+        class="comment-list"
+      >
+        <v-list-item-content>
+          <div class="color-question-name">
+            {{ question.name }}
+          </div>
+        </v-list-item-content>
+
+        <v-list-item-action>
+          <v-icon color="#E8114b" size="30">mdi-chevron-right</v-icon>
+        </v-list-item-action>
+      </v-list-item>
+      
+      <v-divider></v-divider>
+    </v-list>
+  </v-card>
+</template>
+<script>
+export default {
+  props: {
+    questions: { type: Array, default: () => [] }
+  }
+}
+</script>
+<style lang="css">
+.comment-list > .v-list__tile {
+  height: 100% !important;
+  padding: 10px 10px !important;
+}
+
+.color-question-name {
+  color: #303030
+}
+</style>
