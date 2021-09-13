f8ae095bddd5cf11a44c5251386cf663270b7e23
javierc22 <javier.ec.22@gmail.com>
2021-09-13 11:22:02 -0300

    Add component CardInfoA and CardInfoB

---
+++ components/helpcenter/CardInfoA.vue
@@ -0,0 +1,32 @@
+<template lang="">
+  <v-card rounded="lg">
+    <v-img
+      src="https://assets-intranet-security.s3.us-east-2.amazonaws.com/helpcenter_img_02.png"
+      aspect-ratio="2.75"
+    ></v-img>
+    
+    <p class="text-h6 ma-0 font-weight-thin hc__blue-text text-center">
+      ¿Necesitas ver tú historial de casos?
+    </p>
+    
+    <v-card-actions class="text-xs-center">
+      <v-btn outlined class="hc__color-2 mx-auto" :to="'/centro-de-ayuda/mis-casos'">
+        Ir a Mis casos
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style lang="css">
+.hc__blue-text {
+  color: #1B0088
+}
+
+.hc__color-2 {
+  color:#E8114b !important
+}
+</style>
---
+++ components/helpcenter/CardInfoB.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-card rounded="lg">
+    <v-img
+      src="https://assets-intranet-security.s3.us-east-2.amazonaws.com/helpcenter_img_01.jpg"
+      aspect-ratio="2.75"
+    ></v-img>
+    
+
+      <p class="text-h6 ma-0 font-weight-thin hc__blue-text text-center">
+        ¿Necesitas más ayuda?
+      </p>
+      <p class="text-subtitle mt-1 text-center">
+        Escríbenos y nos comunicaremos contigo.
+      </p>
+    <v-card-actions class="text-xs-center">
+      <v-btn outlined class="hc__color-2 mx-auto" :to="'/centro-de-ayuda/crear-caso'">
+        Crear un caso
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style lang="css">
+.hc__blue-text {
+  color: #1B0088
+}
+
+.hc__color-2 {
+  color:#E8114b !important
+}
+</style>
