7f27d6b97325557782dc1f410e5d4d24bda8628e
javierc22 <javier.ec.22@gmail.com>
2021-09-15 16:03:13 -0300

    Add bar commented

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,26 @@
 <template>
   <v-app>
     <v-main>
+      <!-- <v-app-bar fixed app color="primary" dark >
+        <v-row>
+          <v-col cols="12" sm="3" class="py-0"></v-col>
+          <v-col cols="12" sm="6" class="py-0">
+            <v-form>
+              <v-container>
+                <v-row>
+                  <v-col cols="12" md="12" class="mt-6 py-0">
+                    <v-text-field
+                      placeholder="Buscar..."
+                      clearable
+                      type="text"
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-form>
+          </v-col>
+        </v-row>
+      </v-app-bar> -->
       <v-container>
         <v-row no-gutters class="d-flex justify-center">
           <v-col cols="12" lg="12" xl="10">
