2d3bf79145fdc8d0ec59886dd5fddbb9a6805ecb
javierc22 <javier.ec.22@gmail.com>
2021-09-20 12:29:38 -0300

    Add SideMenuMobile

---
+++ components/helpcenter/SideMenuMobile.vue
@@ -0,0 +1,89 @@
+<template>
+  <v-row>
+    <v-dialog v-model="dialog" fullscreen hide-overlay transition="dialog-bottom-transition">
+      <template v-slot:activator="{ on }">
+        <v-btn
+          color="white"
+          block
+          v-on="on"
+        >
+          Cambiar de Categoría
+          <v-spacer></v-spacer>
+          <v-icon color="#638d37" size="30">mdi-chevron-down</v-icon>
+        </v-btn>
+      </template>
+      <v-card>
+        <v-toolbar dark color="primary">
+          <v-btn icon dark @click="dialog = false">
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+          <v-toolbar-title>Categorias</v-toolbar-title>
+          <v-spacer></v-spacer>
+        </v-toolbar>
+
+        <v-list>
+          <v-list-group
+            v-for="item in items"
+            :key="item.id"
+            :value="isActive(item.slug)"
+            no-action
+          >
+            <template v-slot:activator>
+              <v-list-item>
+                <v-list-item-avatar size="20">
+                  <img :src="item.image">
+                </v-list-item-avatar>
+                <v-list-item-content>
+                  <v-list-item-title>{{ item.name }}</v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </template>
+
+            <v-list-item
+              v-for="subcategory in item.subcategories"
+              :key="subcategory.id"
+              @click="goTo(item.slug, subcategory.slug)"
+            >
+              <v-list-item-content>
+                <v-list-item-title>{{ subcategory.name }}</v-list-item-title>
+              </v-list-item-content>
+
+              <v-list-item-action>
+                <!-- <v-icon>{{ subcategory.action }}</v-icon> -->
+              </v-list-item-action>
+            </v-list-item>
+          </v-list-group>
+        </v-list>
+      </v-card>
+    </v-dialog>
+  </v-row>
+</template>
+<script>
+import { mapActions } from 'vuex'
+
+export default {
+  data() {
+    return {
+      dialog: false,
+      items: []
+    }
+  },
+  methods: {
+    ...mapActions("helpcenter", ["fetchCategories"]),
+    isActive(slug) {
+      return this.$route.params.category === slug
+    },
+    goTo(categorySlug, subcategorySlug) {
+      this.$router.push(`/${categorySlug}/${subcategorySlug}`)
+      this.dialog = false
+    }
+  },
+  async created() {
+    const res = await this.fetchCategories()
+    this.items = res
+  }
+}
+</script>
+<style lang="">
+  
+</style>
--- layouts/helpcenter.vue
+++ layouts/helpcenter.vue
@@ -5,11 +5,12 @@
         <v-row no-gutters class="d-flex justify-center">
           <v-col cols="12" lg="12" xl="10">
             <v-row>
-              <v-col md="3">
-                <SideMenu />
+              <v-col cols="12" md="3">
+                <SideMenuMobile v-if="$vuetify.breakpoint.mobile" />
+                <SideMenu v-else />
               </v-col>
 
-              <v-col md="8">
+              <v-col cols="12" md="8">
                 <Nuxt />
               </v-col>
             </v-row>
@@ -22,9 +23,11 @@
 
 <script>
 import SideMenu from '~/components/helpcenter/SideMenu.vue'
+import SideMenuMobile from '~/components/helpcenter/SideMenuMobile.vue'
 export default {
   components: { 
-    SideMenu 
+    SideMenu,
+    SideMenuMobile 
   },
 }
 </script>
