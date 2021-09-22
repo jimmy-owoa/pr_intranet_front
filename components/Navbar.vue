3940026e536e45c358f53f3c277266267fe638b1
javierc22 <javier.ec.22@gmail.com>
2021-09-22 11:10:40 -0300

    Add Navbar with search

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,34 +1,46 @@
 <template>
-  <div data-uk-sticky="sel-target: .uk-navbar-container; cls-active: uk-navbar-sticky" class="uk-sticky uk-sticky-fixed uk-sticky-below" style="position: fixed; top: 0px; width: 1905px;">
-    <nav class="uk-navbar-container uk-margin uk-light uk-navbar-sticky">
+  <v-row justify="center">
+    <div data-uk-sticky="sel-target: .uk-navbar-container; cls-active: uk-navbar-sticky" class="uk-sticky uk-sticky-fixed uk-sticky-below" style="position: fixed; top: 0px; width: 1905px;">
+      <nav class="uk-navbar-container uk-margin uk-light uk-navbar-sticky">
         <div class="uk-container">
-          <v-row>
+          <v-row justify="center">
             <v-col cols="8" class="pb-0">
               <form @submit.prevent="">
                 <v-text-field
                   v-model="search"
                   label="¿En qué te ayudamos?"
                   required
+                  :loading="isLoading"
+                  color="white"
                   dark
                   @keyup="searchTimeOut()"
                 ></v-text-field>
               </form>
             </v-col>
           </v-row>
-            <!-- <div data-uk-navbar="" class="uk-navbar">
-                <div class="uk-navbar-left">
-                    <a class="uk-navbar-item uk-logo uk-text-small" href="index.html">MINIMAL</a>
-                </div>
-                <div class="uk-navbar-center uk-visible@m">
-
-                </div>
-                <div class="uk-navbar-right">
-                    <a class="uk-navbar-toggle uk-search-icon uk-icon" href="#search" data-uk-search-icon="" data-uk-toggle=""><svg width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" ratio="1"><circle fill="none" stroke="#000" stroke-width="1.1" cx="9" cy="9" r="7"></circle><path fill="none" stroke="#000" stroke-width="1.1" d="M14,14 L18,18 L14,14 Z"></path></svg></a>
-                </div>
-            </div> -->
         </div>
-    </nav>
-  </div>
+      </nav>
+    </div>
+
+    <v-col cols="8" class="">
+      <v-list v-for="(result, i) in results" :key="i"> 
+        <v-list-item
+          :key="i"
+          :to="`${result.url}`"
+        >
+          <v-list-item-content>
+            <v-list-item-title v-text="result.name"></v-list-item-title>
+          </v-list-item-content>
+
+          <v-list-item-action>
+            <v-icon color="#2196F3" size="30">mdi-chevron-right</v-icon>
+          </v-list-item-action>
+        </v-list-item>
+        
+        <v-divider></v-divider>
+      </v-list>
+    </v-col>
+  </v-row>
 </template>
 <script>
 import { mapActions } from 'vuex'
@@ -75,92 +87,44 @@ export default {
 }
 
 .uk-navbar-container:not(.uk-navbar-transparent) {
-    background: #2196F3;
-    border-bottom: 1px solid #349ff4;
-}
-
-.uk-light, .uk-section-primary:not(.uk-preserve-color), .uk-section-secondary:not(.uk-preserve-color), .uk-tile-primary:not(.uk-preserve-color), .uk-tile-secondary:not(.uk-preserve-color), .uk-card-primary.uk-card-body, .uk-card-primary > :not([class*='uk-card-media']), .uk-card-secondary.uk-card-body, .uk-card-secondary > :not([class*='uk-card-media']), .uk-overlay-primary, .uk-offcanvas-bar {
-    color: rgba(255, 255, 255, 0.8);
+  background: #2196F3;
+  border-bottom: 1px solid #349ff4;
 }
 
 .uk-margin {
-    margin-bottom: 20px;
-}
-
-nav {
-    display: block;
+  margin-bottom: 20px;
 }
 
 @media (min-width: 960px) {
   .uk-container {
-      padding-left: 40px;
-      padding-right: 40px;
+    padding-left: 40px;
+    padding-right: 40px;
   }
 }
 
 @media (min-width: 640px) {
   .uk-container {
-      padding-left: 30px;
-      padding-right: 30px;
+    padding-left: 30px;
+    padding-right: 30px;
   }
 }
 
 .uk-container {
-    box-sizing: content-box;
-    max-width: 1000px;
-    margin-left: auto;
-    margin-right: auto;
-    padding-left: 15px;
-    padding-right: 15px;
+  box-sizing: content-box;
+  max-width: 1000px;
+  margin-left: auto;
+  margin-right: auto;
+  padding-left: 15px;
+  padding-right: 15px;
 }
 
 .uk-container > :last-child {
-    margin-bottom: 0;
+  margin-bottom: 0;
 }
 
 .uk-navbar {
     display: flex;
     position: relative;
-}
-
-
-
-.uk-light .uk-logo, .uk-section-primary:not(.uk-preserve-color) .uk-logo, .uk-section-secondary:not(.uk-preserve-color) .uk-logo, .uk-tile-primary:not(.uk-preserve-color) .uk-logo, .uk-tile-secondary:not(.uk-preserve-color) .uk-logo, .uk-card-primary.uk-card-body .uk-logo, .uk-card-primary > :not([class*='uk-card-media']) .uk-logo, .uk-card-secondary.uk-card-body .uk-logo, .uk-card-secondary > :not([class*='uk-card-media']) .uk-logo, .uk-overlay-primary .uk-logo, .uk-offcanvas-bar .uk-logo {
-    color: #fff;
-}
-
-.uk-navbar-center:not(:only-child) {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    z-index: 990;
-}
-
-.uk-navbar-left, .uk-navbar-right, .uk-navbar-center, .uk-navbar-center-left > *, .uk-navbar-center-right > * {
-    display: flex;
-    align-items: center;
-}
-
-.uk-navbar-container > .uk-container .uk-navbar-right {
-    margin-right: -15px;
-}
-
-.uk-navbar-left, .uk-navbar-right, .uk-navbar-center:only-child {
-    flex-wrap: wrap;
-}
-.uk-navbar-right {
-    margin-left: auto;
-}
-.uk-navbar-left, .uk-navbar-right, .uk-navbar-center, .uk-navbar-center-left > *, .uk-navbar-center-right > * {
-    display: flex;
-    align-items: center;
-}
-.uk-navbar-nav {
-    display: flex;
-    margin: 0;
-    padding: 0;
-    list-style: none;
 }
 
 .uk-logo {
@@ -173,38 +137,19 @@ nav {
     font-weight: 800;
 }
 
-li {
-    display: list-item;
-    text-align: -webkit-match-parent;
-}
-
-.uk-light .uk-navbar-toggle {
-    color: rgba(255, 255, 255, 0.7);
-}
-
 .uk-icon {
-    margin: 0;
-    border: none;
-    border-radius: 0;
-    overflow: visible;
-    font: inherit;
-    color: inherit;
-    text-transform: none;
-    padding: 0;
-    background-color: transparent;
-    display: inline-block;
-    fill: currentcolor;
-    line-height: 0;
+  margin: 0;
+  border: none;
+  border-radius: 0;
+  overflow: visible;
+  font: inherit;
+  color: inherit;
+  text-transform: none;
+  padding: 0;
+  background-color: transparent;
+  display: inline-block;
+  fill: currentcolor;
+  line-height: 0;
 }
 
-a, .uk-link {
-    color: #2196F3;
-    text-decoration: none;
-    cursor: pointer;
-}
-
-.uk-navbar-left, .uk-navbar-right, .uk-navbar-center, .uk-navbar-center-left > *, .uk-navbar-center-right > * {
-    display: flex;
-    align-items: center;
-}
 </style>
